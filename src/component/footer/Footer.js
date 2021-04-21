9f10a808f2db46dcdb8bc7b3be07de14279da7d9
Vu Duong <19521110@gm.uit.edu.vn>
2021-04-21 22:26:59 +0700

    update footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-900">
-      <div className="container mx-auto text-center text-white py-8">
-        <p>© 2021 iLearn</p>
-        <p>Power by iHelloWorld</p>
+    <footer style={{ backgroundColor: "#14254a" }} className="">
+      <div className="container mx-auto text-white py-8 flex items-baseline flex-wrap">
+        <div className="mx-2 flex items-baseline">
+          <Link to="/">
+            <h2 className="text-white font-semibold text-3xl md:mx-2">
+              <span className="text-blue-500">i</span>Learn
+            </h2>
+          </Link>
+          <p>© 2021 iLearn</p>
+          <p>Power by iHelloWorld</p>
+        </div>
+
+        <ul className="flex md:ml-auto items-baseline justify-center h-full">
+          <Link to="/">
+            <li className="mx-2 text-white">Home</li>
+          </Link>
+          <Link to="/new">
+            <li className="mx-2 text-white">New class</li>
+          </Link>
+          <Link to="/trending">
+            <li className="mx-2 text-white">Trending</li>
+          </Link>
+          <Link to="/about">
+            <li className="mx-2 text-white">About us</li>
+          </Link>
+        </ul>
       </div>
     </footer>
   );
